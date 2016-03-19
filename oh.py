07c8bcf2060b0a697ec8eb6c8280eb4bea203538
jaehyeokjin <jaehyeok.jin.2@gmail.com>
2016-03-19 03:42:09 -0500

    Constructing the oxygen_label

--- oh.py
+++ oh.py
@@ -1,9 +1,12 @@
-f=open('simulate.trj','r')
+f = open('simulate.trj', 'r')
 f.readlines(38)
 arr = f.readlines(575)
+oxygen_label = [0 for _ in range(0, 576)]
+count = 0
 for item in arr:
     a = item.split()
-    if a[1]=='1':
-        print 'O'
+    if a[1] == '1':
+        oxygen_label[count] = 1
     else:
-        print 'H'
+        oxygen_label[count] = 0
+    count += 1
